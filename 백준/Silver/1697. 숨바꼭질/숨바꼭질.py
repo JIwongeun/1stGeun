b1b34cbd5a34a207628dfb112d9061a9eb86baf0
JIwongeun <jiwongeun321@naver.com>
2024-07-26 15:40:38 +0900

    [Silver I] Title: 숨바꼭질, Time: 100 ms, Memory: 35660 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+MAX = 10 ** 5
+
+def bfs(n:int, k:int, visited:dict):
+    q=deque()
+    q.append(n)
+    
+    while q:
+        cur=q.popleft()
+        if cur==k:
+            return visited[cur]
+        for i in (cur-1, cur+1, cur*2):
+            if 0<=i<=MAX and not visited[i]:
+                visited[i]=visited[cur]+1
+                q.append(i)
+
+def main():
+
+    input=sys.stdin.readline
+
+    n,k=map(int, input().split())
+    
+    visited = [0] * (MAX + 1)
+
+    print(bfs(n,k, visited))
+                
+if __name__ == "__main__":
+    main()
