7b7ad6de3cb102a958265ff63c0ed2226dc1013e
JIwongeun <jiwongeun321@naver.com>
2024-08-14 20:17:35 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 80 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,22 @@
+import sys
+
+
+def main():
+    input = sys.stdin.readline
+
+    N=int(input().strip())
+
+    arr=list(map(int, input().split()))
+
+    dp=[1]*N
+
+    for i in range(1, N):
+        for j in range(i):
+            if arr[i]>arr[j]:
+                dp[i]=max(dp[i], dp[j]+1)
+                
+    print(max(dp))
+
+
+if __name__ == "__main__":
+    main()
