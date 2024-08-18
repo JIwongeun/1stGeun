dcb6c16b80674d9ab26015325ae4c37439a3b94f
JIwongeun <jiwongeun321@naver.com>
2024-08-18 23:50:49 +0900

    [Silver III] Title: 회전하는 큐, Time: 64 ms, Memory: 34168 KB -BaekjoonHub

---
+++ 백준/Silver/1021. 회전하는 큐/회전하는 큐.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+def main():
+  
+    input=sys.stdin.readline
+
+    n,m = map(int,input().split())
+
+    data = deque([i for i in range(1,n+1)])
+
+    index = list(map(int,input().split()))
+
+    count = 0
+    for num in index:
+        while 1:
+            if data[0] == num:
+                data.popleft()
+                break
+            else:
+                if data.index(num) <= len(data)//2:
+                    data.rotate(-1)
+                    count += 1
+                else:
+                    data.rotate(1)
+                    count += 1
+
+    print(count)
+        
+if __name__ == "__main__":
+    main()
